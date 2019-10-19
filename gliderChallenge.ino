19d4b3fc2518f0b9149bed35d443eee189973d3c
Republik631 <47197794+Republik631@users.noreply.github.com>
2019-10-19 15:09:08 -0500

    Add files via upload

--- gliderChallenge.ino
+++ gliderChallenge.ino
@@ -4,87 +4,84 @@
  * Brennan Haralson, Aninoritsela Okorodudu, Matthew Wilson, Hughston Turner, Ben Lambert
  * Mentored by Joseph Hayes and Morenike Doherty
  */
-#include <Wire.h> 
+#include <Wire.h>
 #include "ICM_20948.h"
 #include "Adafruit_BMP3XX.h"
 #include <Adafruit_Sensor.h>
 
 // BMP388 I2C
-#define SEALEVELPRESSURE_HPA (1014.2)
-Adafruit_BMP3XX bmp;
+#define SEALEVELPRESSURE_HPA (1012.0) // Must be updated the day flight.
+Adafruit_BMP3XX bmp;   // Create a Adafruit_BMP3XX object.
 
 // ICM20948
 #define WIRE_PORT Wire  // Your desired Wire port.
 #define AD0_VAL   1     // The value of the last bit of the I2C address.
-ICM_20948_I2C myICM;  // Otherwise create an ICM_20948_I2C object
+ICM_20948_I2C myICM;  // Create an ICM_20948_I2C object.
 
 // Teensy4.0
-int brightLED = 13; // Or whatever pin we use for the leds
+int brightLED = 2; // Or whatever pin we use for the leds
 int hotwirePin = 3; // Again, not sure if this is the pin we'll use yet
 
 // Global variables
-bool HOTWIRE_ACTIVATED = false;
-bool HOTWIRE_FINISHED = false;
-bool HOTWIRE_SUCCESS = false;
-float desiredAlt = 290;     // Drop altitude in meters
-float recordTime = 0;
-float altTime = 0;
-float ledTime = 0;
-float hotwireTime = 0;
-float averageCounter = 0;
-float averageAltitude = 0;
-float dropAlt = 0;
+bool HOTWIRE_ACTIVATED = false;        // State of power to hotwire
+bool HOTWIRE_FINISHED = false;         // States whether the hotwire has been run
+bool HOTWIRE_SUCCESS = false;          // State of succes of the hotwire burn
+float localAlt = 200;                  // Local elevation
+float desiredAlt = localAlt + 290;     // Drop altitude in meters
+float recordTime = 0;                  // Time since last recording
+float altTime = 0;                     // Time since last altitude reading
+float ledTime = 0;                     // Time since last LED state change
+float hotwireTime = 0;                 // Time since hotwire was activated
+float averageCounter = 0;              // Accumulator to track number of altitude readings taken
+float averageAltitude = 0;             // Accumulator used for finding average altitude
+float dropAlt = 0;                     // Altitude at whih hotwire is deactivated
 
 
 void setup() {
-  
-  Serial.begin(115200);
-  while (!Serial);
-  Serial.println("BMP388 test");
 
+  // Openlog Setup
+  Serial1.begin(9600); //9600bps is default for OpenLog
+  delay(1000); //Wait a second for OpenLog to init
+  Serial1.println("GLIDER CHALLENGE TEAM 13");
+  Serial1.println("Data is collected every 500 milliseconds.");
+  Serial1.println();
+
+  // BMP388 Initialization
+  Serial1.print("BMP388 initialization: ");
   if (!bmp.begin(0x77)) {
-    Serial.println("Could not find a valid BMP3 sensor, check wiring!");
+    Serial1.println("FAILED!");
     while (1);
   }
+  Serial1.println("SUCCESS!");
+  
   // BMP388 Set up oversampling and filter initialization
   bmp.setTemperatureOversampling(BMP3_OVERSAMPLING_8X);
   bmp.setPressureOversampling(BMP3_OVERSAMPLING_4X);
   bmp.setIIRFilterCoeff(BMP3_IIR_FILTER_COEFF_3);
 
-while(!Serial){};
-  // ICM20948 Setup
+  // ICM20948 Initialization
   WIRE_PORT.begin(0x68);
   WIRE_PORT.setClock(400000);
-  bool initialized = false;
-  
-  while( !initialized ){
 
-    myICM.begin( WIRE_PORT, AD0_VAL );
-
-    Serial.print( F("Initialization of the sensor returned: ") );
-    Serial.println( myICM.statusString() );
-    if( myICM.status != ICM_20948_Stat_Ok ){
-      Serial.println( "Trying again..." );
-      delay(500);
-    }else{
-      initialized = true;
-    }
-
+  Serial1.print("ICM20948 initialization: ");
+  myICM.begin( WIRE_PORT, AD0_VAL );
+  if( myICM.status != ICM_20948_Stat_Ok ){
+    Serial1.println("FAILED!");
+    while(1);
   }
-
-  // Openlog Setup
-  Serial.begin(9600); //9600bps is default for OpenLog
-  delay(1000); //Wait a second for OpenLog to init
-  Serial.println("Glider Challenge Team 13 test"); 
+  Serial1.println("SUCCESS!");   
+  Serial1.println();
 
 
   // Teensy4.0 Setup
   pinMode(brightLED, OUTPUT); 
 
+  // Begin timers
   recordTime = millis();
   ledTime = millis();
   altTime = millis();
-  
+
+  // This loop manages all events up to and including a successful hotwire activation.
   while (!HOTWIRE_SUCCESS) {
     
     // Record data every .5 seconds
@@ -95,156 +92,142 @@ while(!Serial){};
 
     // Check if the glider is above desiredAlt for a period of .1 seconds.
     if ((millis() - altTime >= 50) && (HOTWIRE_ACTIVATED == false) && (HOTWIRE_FINISHED == false)) {
-      
+
+      // Add the current altitude to the accumulator and increment the counter.
       averageAltitude += checkAltitude();
       averageCounter++;
 
-      // Effectively checks the average altitude over a duration of .5 seconds
+      // Effectively checks the average altitude over a duration of .5 seconds.
       if ((averageCounter == 10) && (averageAltitude / averageCounter >= desiredAlt)) {
+        // If the altitude is above desiredAlt, activate the hotwire and begin the hotwire timer.
         hotwireON();
         hotwireTime = millis();
+        // Reset acumulators (they are used in the next function).
         averageAltitude = 0;
         averageCounter = 0;
-        
+
+      // Reset the average altitude counter if the desired altitude is not reached over an interval of .5 seconds
       } else if ((averageCounter == 10) && (averageAltitude / averageCounter < desiredAlt)) {
         averageAltitude = 0;
         averageCounter = 0;
       }
-        
+
+      // Reset the altTime counter
       altTime = millis();      
     }
 
-    // Turn off hotwire after 6 seconds.
+    // Chekc the state of the hotwire and turn it off after 6 seconds.
     if ((HOTWIRE_ACTIVATED == true) && (millis() - hotwireTime >= 6000)) {
       hotwireOFF();
       HOTWIRE_FINISHED = true;
-
+      
+      // Record the altitude at which the hotwire is deactivated
       dropAlt = checkAltitude();
     }
 
     // Check if glider is descending over interval of 1 second.
     if ((millis() - altTime >= 100) && (HOTWIRE_ACTIVATED == false) && (HOTWIRE_FINISHED == true)) {
-    
+
+      // Add the current altitude to the accumulator and increment the counter.
       averageAltitude += checkAltitude();
       averageCounter++;
 
       // Effectively checks the average altitude over a duration of 1 second
       if ((averageCounter == 10) && (averageAltitude / averageCounter < dropAlt)) {
+        // If the average altitude over an interval of 1 second is less than the drop altitude, 
+        // then the hotwire burn was successful.
         HOTWIRE_SUCCESS = true;
+        // Reset acumulators
         averageAltitude = 0;
         averageCounter = 0;
         
       } else if ((averageCounter == 10) && (averageAltitude / averageCounter >= dropAlt)) {
+        // If the average altitude over an interval of 1 second is greater than the drop altitude, 
+        // then the hotwire burn failed. Reset the state variables (so another burn can be done).
         HOTWIRE_FINISHED = false;
+        // Reset acumulators
         averageAltitude = 0;
         averageCounter = 0;
       }      
     }
 
+    // Blinks the LED on intervals specified in the blinkLED function.
     blinkLED();
     
   }
 }
 
-
+// After a successful hotwire burn, the loop function takes over. 
 void loop() {
+
+  // Record data every .5 seconds
   if (millis() - recordTime >= 500) {
     recordData();
     recordTime = millis();
   }
 
+  // Blinks the LED on intervals specified in the blinkLED function.
   blinkLED();
 }
 
 
 
-
+// Check the current altitude.
 float checkAltitude() {
   return(bmp.readAltitude(SEALEVELPRESSURE_HPA));
 }
 
-
+// Write data out to the SD card.
 void recordData() {
-  // Time of recording
-  Serial.print("Elapsed Time: ");
-  Serial.println(millis()-recordTime);
 
   // BMP388 Data Collection
-  Serial.print("Temperature = ");
-  Serial.print(bmp.temperature);
-  Serial.println(" *C");
+  Serial1.print("Temperature = ");
+  Serial1.print(bmp.temperature);
+  Serial1.println(" *C");
 
-  Serial.print("Pressure = ");
-  Serial.print(bmp.pressure / 100.0);
-  Serial.println(" hPa");
+  Serial1.print("Pressure = ");
+  Serial1.print(bmp.pressure / 100.0);
+  Serial1.println(" hPa");
 
-  Serial.print("Altitude = ");
-  Serial.print(bmp.readAltitude(SEALEVELPRESSURE_HPA));
-  Serial.println(" m");
+  Serial1.print("Altitude = ");
+  Serial1.print(bmp.readAltitude(SEALEVELPRESSURE_HPA));
+  Serial1.println(" m");
 
-  Serial.println();
-
-
-  ICM_20948_AGMT_t agmt = myICM.getAGMT();
+  myICM.getAGMT();
   // ICM29048 Data Collection
   // Not sure about formatting yet
-  Serial.print("Scaled. Acc (mg) [ ");
-  printFormattedFloat( myICM.accX(), 5, 2 );
-  Serial.print(", ");
-  printFormattedFloat( myICM.accY(), 5, 2 );
-  Serial.print(", ");
-  printFormattedFloat( myICM.accZ(), 5, 2 );
-  Serial.print(" ], Gyr (DPS) [ ");
-  printFormattedFloat( myICM.gyrX(), 5, 2 );
-  Serial.print(", ");
-  printFormattedFloat( myICM.gyrY(), 5, 2 );
-  Serial.print(", ");
-  printFormattedFloat( myICM.gyrZ(), 5, 2 );
-  Serial.print(" ], Mag (uT) [ ");
-  printFormattedFloat( myICM.magX(), 5, 2 );
-  Serial.print(", ");
-  printFormattedFloat( myICM.magY(), 5, 2 );
-  Serial.print(", ");
-  printFormattedFloat( myICM.magZ(), 5, 2 );
-  Serial.print(" ], Tmp (C) [ ");
-  printFormattedFloat( myICM.temp(), 5, 2 );
-  Serial.print(" ]");
-  Serial.println();
+  Serial1.print("Scaled. Acc (mg) [ ");
+  Serial1.print(myICM.accX());
+  Serial1.print(", ");
+  Serial1.print(myICM.accY());
+  Serial1.print(", ");
+  Serial1.print(myICM.accZ());
+  Serial1.print(" ], Gyr (DPS) [ ");
+  Serial1.print(myICM.gyrX());
+  Serial1.print(", ");
+  Serial1.print(myICM.gyrY());
+  Serial1.print(", ");
+  Serial1.print(myICM.gyrZ());
+  Serial1.print(" ], Mag (uT) [ ");
+  Serial1.print(myICM.magX());
+  Serial1.print(", ");
+  Serial1.print(myICM.magY());
+  Serial1.print(", ");
+  Serial1.print(myICM.magZ());
+  Serial1.print(" ], Tmp (C) [ ");
+  Serial1.print(myICM.temp());
+  Serial1.println(" ]");
+
+  // Time of recording
+  Serial1.print("Elapsed time since last recording: ");
+  Serial1.println(millis()-recordTime);
+  Serial1.println();
   
 }
 
-// Only used to format the ICM data. MAY NOT BE NECESSARY!
-void printFormattedFloat(float val, uint8_t leading, uint8_t decimals){
-  float aval = abs(val);
-  if(val < 0){
-    Serial.print("-");
-  }else{
-    Serial.print(" ");
-  }
-  for( uint8_t indi = 0; indi < leading; indi++ ){
-    uint32_t tenpow = 0;
-    if( indi < (leading-1) ){
-      tenpow = 1;
-    }
-    for(uint8_t c = 0; c < (leading-1-indi); c++){
-      tenpow *= 10;
-    }
-    if( aval < tenpow){
-      Serial.print("0");
-    }else{
-      break;
-    }
-  }
-  if(val < 0){
-    Serial.print(-val, decimals);
-  }else{
-    Serial.print(val, decimals);
-  }
-}
-
-// Blink LED
+// Blink LED on for 400 milliseconds, off for 1200 milliseconds.
 void blinkLED() {
-  if((digitalRead(brightLED) == LOW) && (millis() - ledTime >= 1200)) {//Turn the status LED on/off as we go
+  if((digitalRead(brightLED) == LOW) && (millis() - ledTime >= 1200)) {
     digitalWrite(brightLED, HIGH);
     ledTime = millis();
     
